a35225d0301491a90260d4897e7e09586011c90d
Herleybob <56013441+Herleybob@users.noreply.github.com>
2020-03-10 01:55:17 -0700

    2

--- octoprint_printerpoweronoff/static/js/printerpoweronoff.js
+++ octoprint_printerpoweronoff/static/js/printerpoweronoff.js
@@ -6,50 +6,17 @@ $(function() {
     function printerpoweronoffViewModel(parameters) {
         var self = this;
 
-        self.loginState = parameters[0];
-        self.printerState = parameters[1];
-        self.settings = parameters[2];
+        self.printerpowerOff = function () {
 
-        self.estopCommand = ko.observable("M112");
+        };
 
-		self.enableEstop = ko.pureComputed(function() {
-            return self.printerState.isOperational() && self.loginState.isUser();
-        });
+        self.printerpowerOn = function () {
 
-        self.estopState = ko.pureComputed(function() {
-            return self.loginState.isUser() > 0 ? "estop_sidebar" : "estop_sidebar_disabled";
-        });
-
-        self.buttonText = ko.pureComputed(function() {
-            if (self.enableEstop()) {
-                return gettext("EMERGENCY STOP");
-            } else {
-                return gettext("Offline");
-            }
-        });
-
-        self.buttonTitle = ko.pureComputed(function() {
-            self.estopCommand(self.settings.settings.plugins.estop.estopCommand());
-            return gettext("Sends " + self.estopCommand() + " to the printer IMMEDIATELY");
-        });
-
-        self.onBeforeBinding = function () {
-            //self.estopCommand(self.settings.settings.plugins.estop.estopCommand());
-        }
-		self.sendEstopCommand = function () {
-			if (self.enableEstop()) {
-                self.estopCommand(self.settings.settings.plugins.printerpoweronoff.estopCommand());
-                OctoPrint.control.sendGcode(self.estopCommand());
-			};
         };
     }
 
 	OCTOPRINT_VIEWMODELS.push({
         construct: printerpoweronoffViewModel,
-        dependencies: [
-			"loginStateViewModel",
-			"printerStateViewModel",
-			"settingsViewModel"],
         elements: ["#sidebar_plugin_printerpoweronoff_wrapper"]
     });
 });
