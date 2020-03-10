868b2c34e07488198d00a1821708ad8d32be49c4
Herleybob <56013441+Herleybob@users.noreply.github.com>
2020-03-10 11:06:33 -0700

    1

--- octoprint_printerpoweronoff/static/js/printerpoweronoff.js
+++ octoprint_printerpoweronoff/static/js/printerpoweronoff.js
@@ -6,12 +6,15 @@ $(function() {
     function printerpoweronoffViewModel(parameters) {
         var self = this;
 
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+
         self.printerpowerOff = function () {
           self.GPIO.output(23, self.GPIO.HIGH)
         };
 
         self.printerpowerOn = function () {
-
+          self.GPIO.output(23, self.GPIO.LOW)
         };
     }
 
